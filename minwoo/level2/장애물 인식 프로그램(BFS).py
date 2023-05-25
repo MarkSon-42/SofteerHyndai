0c998eeb747986a1daf9d2725a7837f769cbd288
minwoo <mark42son@gmail.com>
2023-05-25 21:03:50 +0900

    upload code

--- minwoo/level2/장애물 인식 프로그램(BFS).py
+++ minwoo/level2/장애물 인식 프로그램(BFS).py
@@ -1,1 +1,56 @@
+# comments by chatGPT
+
+# deque 클래스를 불러옴. 이는 BFS에 필요한 큐(queue)를 구현하기 위함입니다.
+from collections import deque
+
+# Python의 표준 입력 라이브러리를 불러옴.
 import sys
+
+# 입력을 더 빠르게 받기 위해 sys.stdin.readline()을 사용함.
+input = sys.stdin.readline
+
+# 사용자로부터 정사각형 맵의 크기 n을 입력 받음.
+n = int(input())
+
+# 그래프를 입력 받음. 각 줄을 rstrip()으로 끝의 개행문자를 제거하고, map 함수로 int형으로 변환한 뒤, 리스트로 만듬.
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+
+# 결과를 담을 리스트 초기화.
+result = []
+
+# 네 방향(상하좌우)을 탐색하기 위한 좌표의 변화값을 정의.
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# bfs 함수를 정의.
+def bfs(i, j):
+    # 빈 deque를 생성함. 이는 BFS에 사용될 큐(queue)임.
+    queue = deque()
+    queue.append((i, j)) # 시작 노드를 큐에 넣음.
+    graph[i][j] = 0 # 시작 노드를 방문 표시.
+    cnt = 1 # 방문한 노드의 수를 세기 위한 카운터 초기화.
+
+    while queue: # 큐가 빌 때까지
+        x, y = queue.popleft() # 큐에서 노드를 하나 꺼냄.
+        for k in range(4): # 네 방향에 대해
+            nx = x + dx[k] # 다음 노드의 x 좌표를 계산
+            ny = y + dy[k] # 다음 노드의 y 좌표를 계산
+            # 만약 다음 노드가 그래프의 범위 내에 있고 아직 방문하지 않은 노드라면
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+                queue.append((nx, ny)) # 큐에 넣고
+                graph[nx][ny] = 0 # 방문 표시를 하고
+                cnt += 1 # 카운터를 증가시킴.
+    return cnt # 모든 인접한 노드를 방문한 후, 방문한 노드의 수를 반환함.
+
+# 그래프의 모든 노드에 대해
+for i in range(n):
+    for j in range(n):
+        # 만약 해당 노드가 아직 방문하지 않은 노드라면
+        if graph[i][j] == 1:
+            result.append(bfs(i, j)) # BFS를 수행하고 그 결과를 결과 리스트에 추가함.
+
+# 결과를 정렬함.
+result.sort()
+print(len(result))
+for i in result:
+    print(i)
--- minwoo/level2/장애물 인식 프로그램(DFS).py
+++ minwoo/level2/장애물 인식 프로그램(DFS).py
@@ -1,1 +1,45 @@
-import sys
+# dfs로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+def dfs(x, y):
+    # 주어진 범위를 벗어나는 경우에는 종료
+    if x <= -1 or x >= N or y <= -1 or y >= N:
+        return False
+    # 현재 노드들 아직 방문하지 않았다면
+    if graph[x][y] == 1:
+        # 장애물의 개수 체크
+        cnt.append(1)
+        # 해당 노드 방문 처리
+        graph[x][y] = 0
+        # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+
+
+N = int(input())
+cnt = []
+
+graph = []
+# 모든 위치(노드)에 대하여 장애물 블록을 만듬.
+for i in range(N):
+    graph.append(list(map(int, input())))
+
+result = 0
+result_list = []
+for i in range(N):
+    for j in range(N):
+    # 현재 위치에서 DFS수행
+        if dfs(i, j) == True:
+            result += 1
+            # 길이를 통해 장애물의 개수 확인
+            result_list.append(len(cnt))
+            cnt = []
+
+# 장애물의 수 오름차순 정렬 후 출력
+result_list.sort()
+for i in result_list:
+    print(i)
+
